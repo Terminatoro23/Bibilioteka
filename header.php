89ad72f67eed3c58aa3292c8a9126bb8dbd5f5f6
Terminatoro23 <64741935+Terminatoro23@users.noreply.github.com>
2021-03-02 13:43:26 +0100

    Fix bugs

---
+++ header.php
@@ -0,0 +1,24 @@
+<?php
+echo'<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="panele.css"/>
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+<a href="index.php">Strona główna</a>
+<a href="';
+  session_start();
+  if($_SESSION['typ']=='bibliotekarz'){
+    echo 'panel_bibliotekarza.php';
+  }
+  else{
+    echo 'panel_klienta.php';
+  }
+echo '">Wróć</a>';
+?>
